3510d2426519f68e40abe115dc2acf83f7f51811
Your Name <helenemartin13@hotmail.com>
2020-09-20 13:54:00 +0100

    make and pass first test for sumMultiples exercise 006

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -6,3 +6,16 @@ const {
     createMatrix,
     areWeCovered
   } = require("../challenges/exercise006");
+
+describe("sumMultiples", () => {
+  test ("return the sum of any number that are multiples of 3 or 5", () => {
+    const result = sumMultiples([1,3,5]);
+    const expected = 8;
+    expect (result).toBe(expected);
+
+  });
+
+  // test ("return the sum of any decimal numbers that are multiples of 3 and 5")
+
+
+});
