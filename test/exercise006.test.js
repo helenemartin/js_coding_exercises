7dfac49fb651f72d319853b9ddfbcc932780d295
Your Name <helenemartin13@hotmail.com>
2020-09-20 14:04:33 +0100

    pass second test on sumMultiples exercise

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -15,7 +15,12 @@ describe("sumMultiples", () => {
 
   });
 
-  // test ("return the sum of any decimal numbers that are multiples of 3 and 5")
+  test ("return the sum of any decimal numbers that are multiples of 3 and 5", () => {
+    const result = sumMultiples([1, 3, 5, 15.0, 2.567, 3.33]);
+    const expected = 23;
+    expect(result).toBe(expected);
+
+  });
 
 
 });
