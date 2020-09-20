df7645f0edd9d75113535cd798ba0905cbd00078
Your Name <helenemartin13@hotmail.com>
2020-09-20 18:44:42 +0100

    implement test for sumDigits exercises

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -5,3 +5,17 @@ const {
     hexToRGB,
     findWinner
   } = require("../challenges/exercise007");
+
+  describe("sumDigits", () => {
+    test("return error with an empty argument", () => {
+        expect(() => { sumDigits(); }).toThrow("n is required");
+    });
+    test("return same number for single digit", () => {
+        expect(sumDigits(2)).toBe(2);
+    });
+    test("return sum of multiple digits", () => {
+        expect(sumDigits(123)).toBe(6);
+        expect(sumDigits(49785)).toBe(33);
+        expect(sumDigits(1206)).toBe(9);
+    });
+});
