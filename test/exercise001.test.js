2fdab1db0e699ab4fee7199c086547fef2cae8fa
Your Name <helenemartin13@hotmail.com>
2020-11-05 18:22:34 +0000

    pass all tests

--- test/exercise001.test.js
+++ test/exercise001.test.js
@@ -12,75 +12,75 @@ const {
 } = require("../challenges/exercise001");
 
 describe("capitalize", () => {
-  xtest("returns a capitalized string", () => {
+  test("returns a capitalized string", () => {
     expect(capitalize("hello")).toBe("Hello");
     expect(capitalize("the quick fox")).toBe("The quick fox");
     expect(capitalize("oh no, bears!!!")).toBe("Oh no, bears!!!");
   });
 
-  xtest("does nothing if the string is already capitalized", () => {
+  test("does nothing if the string is already capitalized", () => {
     expect(capitalize("Hello")).toBe("Hello");
   });
 });
 
 describe("generateInitials", () => {
-  xtest("returns the initials of a firstname and surname", () => {
+  test("returns the initials of a firstname and surname", () => {
     expect(generateInitials("Frederic", "Bonneville")).toBe("F.B");
   });
 });
 
 describe("addVAT", () => {
-  xtest("adds a VAT of 20% to a price of 100", () => {
+  test("adds a VAT of 20% to a price of 100", () => {
     expect(addVAT(100, 20)).toBe(120);
   });
 
-  xtest("adds a VAT of 17.5% to a price of 40", () => {
+  test("adds a VAT of 17.5% to a price of 40", () => {
     expect(addVAT(40, 17.5)).toBe(47);
   });
 
-  xtest("adds a VAT of 17.5% to a price of 33.50", () => {
+  test("adds a VAT of 17.5% to a price of 33.50", () => {
     expect(addVAT(33.5, 17.5)).toBe(39.36);
   });
 
-  xtest("adds a VAT of 0% to a price of 25", () => {
+  test("adds a VAT of 0% to a price of 25", () => {
     expect(addVAT(25, 0)).toBe(25);
   });
 });
 
 describe("getSalePrice", () => {
-  xtest("reduces a price of 100 by 50%", () => {
+  test("reduces a price of 100 by 50%", () => {
     expect(getSalePrice(100, 50)).toBe(50);
   });
 
-  xtest("reduces a price of 100 by 33.3%", () => {
+  test("reduces a price of 100 by 33.3%", () => {
     expect(getSalePrice(100, 33.3)).toBe(66.7);
   });
 
-  xtest("reduces a price of 79.99 by 15%", () => {
+  test("reduces a price of 79.99 by 15%", () => {
     expect(getSalePrice(79.99, 15)).toBe(67.99);
   });
 
-  xtest("reduces a price of 50 by 0%", () => {
+  test("reduces a price of 50 by 0%", () => {
     expect(getSalePrice(50, 0)).toBe(50);
   });
 });
 
 describe("getMiddleCharacter", () => {
-  xtest("returns the middle character from a string of odd length", () => {
+  test("returns the middle character from a string of odd length", () => {
     expect(getMiddleCharacter("bears!!!!")).toBe("s");
   });
 
-  xtest("returns the middle 2 characters from a string of even length", () => {
+  test("returns the middle 2 characters from a string of even length", () => {
     expect(getMiddleCharacter("help!!")).toBe("lp");
   });
 });
 
 describe("reverseWord", () => {
-  xtest("returns the provided word, reversed", () => {
+  test("returns the provided word, reversed", () => {
     expect(reverseWord("foo")).toBe("oof");
   });
 
-  xtest("returns a longer sentence, reversed", () => {
+  test("returns a longer sentence, reversed", () => {
     expect(reverseWord("why would you even want to do this?")).toBe(
       "?siht od ot tnaw neve uoy dluow yhw"
     );
@@ -88,11 +88,11 @@ describe("reverseWord", () => {
 });
 
 describe("reverseAllWords", () => {
-  xtest("reverses a single word in an array", () => {
+  test("reverses a single word in an array", () => {
     expect(reverseAllWords(["jest"])).toEqual(["tsej"]);
   });
 
-  xtest("reverses a multiple words in an array", () => {
+  test("reverses a multiple words in an array", () => {
     expect(
       reverseAllWords(["jest", "mocha", "rspec", "jasmine", "selenium"])
     ).toEqual(["tsej", "ahcom", "cepsr", "enimsaj", "muineles"]);
@@ -100,7 +100,7 @@ describe("reverseAllWords", () => {
 });
 
 describe("countLinuxUsers", () => {
-  xtest("returns 0 if no Linux users found", () => {
+  test("returns 0 if no Linux users found", () => {
     const users = [
       { name: "Heather", OS: "Windows 8", type: "Windows" },
       { name: "Paul", OS: "Firefox OS", type: "Unknown" },
@@ -110,7 +110,7 @@ describe("countLinuxUsers", () => {
     expect(countLinuxUsers(users)).toBe(0);
   });
 
-  xtest("returns the correct number of Linux users found", () => {
+  test("returns the correct number of Linux users found", () => {
     const users = [
       { name: "Heather", OS: "Ubuntu 18.04", type: "Linux" },
       { name: "Paul", OS: "Ubuntu 16.04", type: "Linux" },
@@ -125,30 +125,30 @@ describe("countLinuxUsers", () => {
 });
 
 describe("getMeanScore", () => {
-  xtest("returns the mean score from an array of scores", () => {
+  test("returns the mean score from an array of scores", () => {
     expect(getMeanScore([8, 9, 7])).toBe(8);
     expect(getMeanScore([88, 86, 93])).toBe(89);
   });
 
-  xtest("returns the mean to 2 decimal places", () => {
+  test("returns the mean to 2 decimal places", () => {
     expect(getMeanScore([24, 44, 56, 11, 12, 17, 34])).toBe(28.29);
   });
 });
 
 describe("simpleFizzBuzz", () => {
-  xtest("returns 'fizz' if the number is divisible by 3", () => {
+  test("returns 'fizz' if the number is divisible by 3", () => {
     expect(simpleFizzBuzz(3)).toBe("fizz");
   });
 
-  xtest("returns 'buzz' if the number is divisible by 5", () => {
+  test("returns 'buzz' if the number is divisible by 5", () => {
     expect(simpleFizzBuzz(5)).toBe("buzz");
   });
 
-  xtest("returns the number if the number is divisible by neither 3 nor 5", () => {
+  test("returns the number if the number is divisible by neither 3 nor 5", () => {
     expect(simpleFizzBuzz(4)).toBe(4);
   });
 
-  xtest("returns 'fizzbuzz' if the number is divisible by 3 and 5", () => {
+  test("returns 'fizzbuzz' if the number is divisible by 3 and 5", () => {
     expect(simpleFizzBuzz(15)).toBe("fizzbuzz");
   });
 });
